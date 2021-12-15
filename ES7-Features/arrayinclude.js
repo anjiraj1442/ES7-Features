6a4ffa3cc422ebe4f881d667dd4631f0a4fe71e6
anjiraj1442 <anjiraj1442@gmail.com>
2021-12-15 17:17:37 +0530

    Added:es7 features

---
+++ ES7-Features/arrayinclude.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4];
+if(arr.includes(5)) { 
+      console.log('Array contains value');
+     
+     } else{
+          console.log("not contaim any value")
+     }
---
+++ ES7-Features/exponent.js
@@ -0,0 +1,6 @@
+let base =2;
+let exponent = 4;
+ //let result=Math.pow(base,exponent);
+// here ** represents the exponetial operator
+let result = base**exponent;
+ console.log(result);
