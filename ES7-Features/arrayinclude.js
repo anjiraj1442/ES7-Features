ed7e03034c1d4c589837b0588c60e8d4e7ff4926
anjiraj1442 <anjiraj1442@gmail.com>
2021-12-16 09:15:04 +0530

    Added:array include concept is added

--- ES7-Features/arrayinclude.js
+++ ES7-Features/arrayinclude.js
@@ -1,5 +1,14 @@
 let arr = [1, 2, 3, 4];
-if(arr.includes(5)) { 
+if(arr.includes(4)) { 
+      console.log('Array contains value');
+     
+     } else{
+          console.log("not contaim any value")
+     }
+
+     //based on index value find the number
+     let arr = [1, 2, 3, 4];
+if(arr.includes(2,3)) { 
       console.log('Array contains value');
      
      } else{
